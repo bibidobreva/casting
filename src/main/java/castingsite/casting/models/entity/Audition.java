749db5a97b49b99b8f9bb5eedf68a20958aaff37
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2024-04-26 15:49:50 +0300

    new class Audition

---
+++ src/main/java/castingsite/casting/models/entity/Audition.java
@@ -0,0 +1,10 @@
+package castingsite.casting.models.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class Audition extends BaseEntity{
+    
+}

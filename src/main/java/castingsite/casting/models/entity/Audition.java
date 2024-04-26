59319166a4e11350339a8acad744f2a1464dbc51
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2024-04-26 16:02:17 +0300

    small changes

--- src/main/java/castingsite/casting/models/entity/Audition.java
+++ src/main/java/castingsite/casting/models/entity/Audition.java
@@ -1,10 +1,17 @@
 package castingsite.casting.models.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+
+import java.time.LocalDate;
 
 @Table
 @Entity
 public class Audition extends BaseEntity{
-    
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column( nullable = false)
+    private String description;
 }

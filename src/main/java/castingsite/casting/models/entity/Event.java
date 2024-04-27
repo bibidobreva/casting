3828c043fd60f244f1b21de2c9f14b4106636b91
bibidobreva <114009406+bibidobreva@users.noreply.github.com>
2024-04-27 19:43:05 +0300

    Create Event.java

---
+++ src/main/java/castingsite/casting/models/entity/Event.java
@@ -0,0 +1,50 @@
+package castingsite.casting.models.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class Event extends BaseEntity {
+    @Column
+    @Size(min = 2, max = 20)
+    private String name;
+    @Column(columnDefinition = "text")
+    @Size(min = 2, max = 200)
+    private String description;
+
+    @Future
+    private LocalDate date;
+
+
+    public Event(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
